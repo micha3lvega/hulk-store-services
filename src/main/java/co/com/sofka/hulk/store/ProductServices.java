57c3a94e4f852187a22d47432d272534403689ad
micha3lvega <micha3lvega@gmail.com>
2020-02-17 00:20:52 -0500

    inittial commit

---
+++ src/main/java/co/com/sofka/hulk/store/ProductServices.java
@@ -0,0 +1,13 @@
+package co.com.sofka.hulk.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServices {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServices.class, args);
+	}
+
+}
